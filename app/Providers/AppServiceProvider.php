af71f25daa6129690ac350b9c776e20547fb5163
Justine <villarosajustine42@gmail.com>
2025-12-10 09:51:10 +0800

    Force HTTPS scheme to fix mixed content

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <--- ADD THIS LINE
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // FORCE HTTPS IN PRODUCTION
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
