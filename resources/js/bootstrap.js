42e374ac46cdf9a750ad9c594e01a17b07bda80e
Justine <villarosajustine42@gmail.com>
2025-12-10 09:56:17 +0800

    Disable Echo to fix console error

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -17,19 +17,19 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  * for events that are broadcast by Laravel.
  */
 
-import Echo from 'laravel-echo';
-import Pusher from 'pusher-js';
+// import Echo from 'laravel-echo';
+// import Pusher from 'pusher-js';
 
-window.Pusher = Pusher;
+// window.Pusher = Pusher;
 
-window.Echo = new Echo({
-    broadcaster: 'pusher',
-    key: import.meta.env.VITE_PUSHER_APP_KEY,
-    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
-    wsHost: import.meta.env.VITE_PUSHER_HOST ? import.meta.env.VITE_PUSHER_HOST : `window.location.hostname`,
-    wsPort: import.meta.env.VITE_PUSHER_PORT ?? 6001,
-    wssPort: import.meta.env.VITE_PUSHER_PORT ?? 6001,
-    forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
-    enabledTransports: ['ws', 'wss'], // Force WebSockets only
-    disableStats: true, // Stop trying to send stats to real Pusher
-});
+// window.Echo = new Echo({
+//     broadcaster: 'pusher',
+//     key: import.meta.env.VITE_PUSHER_APP_KEY,
+//     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
+//     wsHost: import.meta.env.VITE_PUSHER_HOST ? import.meta.env.VITE_PUSHER_HOST : `window.location.hostname`,
+//     wsPort: import.meta.env.VITE_PUSHER_PORT ?? 6001,
+//     wssPort: import.meta.env.VITE_PUSHER_PORT ?? 6001,
+//     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
+//     enabledTransports: ['ws', 'wss'], // Force WebSockets only
+//     disableStats: true, // Stop trying to send stats to real Pusher
+// });
